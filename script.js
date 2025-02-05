89bd998df437187aca933f07e480e388203ac1d7
Alexandr Shatskyy <autome@ukr.net>
2025-02-05 19:25:51 +0100

    something

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 "use strict";
+const arrQ = [
+	['Learn as if you will live forever, live like you will die tomorrow.', '—Mahatma Gandhi'],
+	['Stay away from those people who try to disparage your ambitions. Small minds will always do that, but great minds will give you a feeling that you can become great too.', '—Mark Twain'],
+	['When you give joy to other people, you get more joy in return. You should give a good thought to the happiness that you can give out.', '—Eleanor Roosevelt'],
+	['Success is not final; failure is not fatal: It is the courage to continue that counts.', '—Winston Churchill'],
+	['Success usually comes to those who are too busy to be looking for it.', '—Henry David Thoreau'],
+	['I never dreamed about success. I worked for it.', '—Estée Lauder'],
+	['The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.', '—Winston Churchill'],
+	['Don’t let yesterday take up too much of today.', '—Will Rogers'],
+	['Experience is a hard teacher because she gives the test first, the lesson afterward.', '—Vernon Sanders Law'],
+	['Either you run the day or the day runs you.', '—Jim Rohn'],
+	['Opportunity is missed by most people because it is dressed in overalls and looks like work.', '—Thomas Edison'],
+	['He who conquers himself is the mightiest warrior.', '—Confucius'],
+	['Try not to become a man of success, but rather become a man of value.', '—Albert Einstein'],
+	['One man with courage makes a majority.', '—Andrew Jackson'],
+	['Education is the most powerful weapon which you can use to change the world.', '—Nelson Mandela'],
+	['Take the attitude of a student, never be too big to ask questions, never know too much to learn something new.', '—Augustine Og Mandino'],
+	['Keep your eyes on the stars, and your feet on the ground.', '—Theodore Roosevelt'],
+	['Work until your bank account looks like a phone number.', '—Unknown'],
+	['Just one small positive thought in the morning can change your whole day.', '—Dalai Lama'],
+	['Do the best you can. No one can do more than that.', '—John Wooden'],
+	['When you arise in the morning, think of what a privilege it is to be alive, to think, to enjoy, to love.', '—Marcus Aurelius'],
+	['Life is like riding a bicycle. To keep your balance, you must keep moving.', '—Albert Einstein'],
+	['Live out of your imagination, not your history.', '—Stephen Covey'],
+	['A year from now you will wish you had started today.', '—Unknown'],
+	['I do not try to dance better than anyone else. I only try to dance better than myself.', '—Arianna Huffington'],
+	['If you don’t risk anything, you risk even more.', '—Erica Jong'],
+	['If it makes you nervous, you’re doing it right.', '—Childish Gambino'],
+	['I choose to make the rest of my life the best of my life.', '—Louise Hay'],
+	['No one changes the world who isn’t obsessed.', '—Billie Jean King']
+]
+
+console.log(arrQ.length);
 const a = document.getElementById("twttr-bttn");
 const d =
   "Just one small positive thought in the morning can change your whole day.";
