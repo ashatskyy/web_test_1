4485385f204967767adcab5fa2e0e2ed1d7c2706
Alexandr Shatskyy <autome@ukr.net>
2025-02-05 10:38:22 +0100

    something

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+const a = document.getElementById("twttr-bttn");
+const d =
+  "Just one small positive thought in the morning can change your whole day.";
+a.href = `https://twitter.com/intent/tweet?text=${d}`;
